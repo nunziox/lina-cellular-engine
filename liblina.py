d6ad57e59532ec7ee08c12baa3bfdcaf72a71c3f
Nunzio Meli <nunzio.meli@openbet.com>
2017-01-29 18:26:24 +0000

    Reduce algorithm complexity to O(n)

--- liblina.py
+++ liblina.py
@@ -7,32 +7,17 @@ __maintainer__ = "Nunzio Meli"
 __email__      = "nunziomeli5@gmail.com"
 __status__     = "Production"
 
+import pygame
+import copy
 
 class GOLEvolutionModel:
   """
   Describes the evolution rules of the cellular grid.
   """
-  #
-  # Patterns
-  #
-  PT_GLIDER        = [[0,1], [1,2], [2,0], [2,1], [2,2]]
-  PT_BLINKER       = [[1,1], [2,1], [3,1]]
-  PT_TRIO          = [[0,1], [1,0], [1,1], [1,2]]
-  PT_PULSAR        = [[0,0], [1,0], [2,0], [3,0], [4,0], [0,2], [4,2], [0,4], [1,4], [2,4], [3,4], [4,4]]
-  
-  PT_GOSPEL        = [[5,1], [5,2], [6,1], [6,2], [5,11], [6,11], [7,11], [4,12], [8,12], [3,13], [3,14], 
-                      [9,13], [9,14], [6,15], [4,16], [8,16], [5,17], [6,17], [7,17], [6,18], [3,21], [4,21],
-                      [5,21], [3,22], [4,22], [5,22], [2,23], [6,23], [1,25], [2,25], [6,25], [7,25], [3,35], 
-                      [4,35], [3,36], [4,36]]
 
-  PT_PERIOD        = [[1,1], [17,1], [1,2], [15,2], [16,2], [17,2], [1,2], [2,2], [3,2], [4,3], [14,3], [3,4],
-                      [4,4], [14,4], [15,4], [5,12], [6,12], [7,12], [11,12], [12,12], [13,12], [5,13], [8,13],
-                      [10,13], [13,13], [5,14], [13,14], [6,16], [12,16], [7,17], [8,17], [10,17], [11,17],
-                      [3,26], [4,26], [3,27], [4,27], [14,26], [15,26], [14,27], [15,27], [7,36], [11,36], 
-                      [7,37], [11,37], [4,40], [5,40], [7,40], [11,40], [13,40], [14,40], [5,41], [6,41],
-                      [7,41], [11,41], [12,41], [13,41], [6,42], [12,42], [3,49], [4,49], [14,49], [15,49],
-                      [4,50], [14,50], [1,51], [2,51], [3,51], [15,51], [16,51], [17,51], [1,52], [17,52]]
-
+#
+# Implements the GOL evolution model
+#
   def check(self, point, wsum):
     """
     This method implements the evolution rules
@@ -58,50 +43,79 @@ class CellularGrid:
   ALIVE = 1 # PIXEL STATE ALIVE
   DEAD  = 0 # PIXEL STATE DEAD
 
-  def __init__(self, model, sizeX, sizeY):
+  def __init__(self, client, model, scale, fps):
+    self.client = client
     self.model = model
-    self.sizeX = sizeX
-    self.sizeY = sizeY
-    self._initBoardLimits()
+    self.scale = scale
+    self.point_list = []
+
+    pygame.init()
+    self.screen   = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    width, height = self.screen.get_size()
+    self.surface  = pygame.Surface(self.screen.get_size())
+    clock         = pygame.time.Clock()
+
+    self.sizeX = width  / self.scale
+    self.sizeY = height / self.scale
     self.array = None
     self.reset()
 
-  def _initBoardLimits(self):
-    self.startX = self.sizeX
-    self.startY = self.sizeY
-    self.finalX = 0
-    self.finalY = 0
+    self.client.init(self)
+
+    while True:
+      self.client.loop(self)
+      for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            exit(0)
+          self.client.keyPressEvent(self)
+      clock.tick(fps)
+
+  #
+  # Shows cellular grid to the screen
+  #
+  def render(self):
+    for elem in self.point_list:
+      color = (0,0,0) if elem[2] == CellularGrid.DEAD else (255,255,255)
+      pygame.draw.rect(self.surface, color, pygame.Rect(elem[1]*self.scale,elem[0]*self.scale,self.scale,self.scale))
+    self.screen.blit(self.surface, (0,0))
+    pygame.display.flip()
 
   def reset(self):
     """
     Allows to reset the grid
     """
+    self.dead_list = []
+    for elem in self.point_list:
+      self.dead_list.append((elem[0],elem[1],CellularGrid.DEAD))
+    self.point_list = []
     self.array = [[0 for x in range(self.sizeX)] for x in range(self.sizeY)]
+    self.surface  = pygame.Surface(self.screen.get_size())
+    self.render()
+    self.point_list = []
 
   def evolve(self):
     """
     Evolves the grid to the next state
     """
+    a = list()
     newpoints = []
-    x0 = self.startX
-    x1 = self.finalX
-    y0 = self.startY
-    y1 = self.finalY
-    self._initBoardLimits()
-    if y1+2 > self.sizeY:
-      y0 = 0
-      y1 = self.sizeY
-    else:
-      y0 = y0 -1
-      y1 = y1 +2
-    if x1+2 > self.sizeX:
-      x0 = 0
-      x1 = self.sizeX
-    else:
-      x0 = x0 -1
-      x1 = x1 +2
-    for i in range(y0,y1):
-      for j in range(x0,x1):
+    for elem in self.point_list:
+      i = elem[0]
+      j = elem[1]
+      t = self.sizeY -1 if i-1 == -1 else i-1
+      k = self.sizeX -1 if j-1 == -1 else j-1
+      a.append((t,j,self.array[t][j]))
+      a.append((t,k,self.array[t][k]))
+      a.append((t,(j+1) % self.sizeX,self.array[t][(j+1) % self.sizeX]))
+      a.append(((i+1) % self.sizeY,j, self.array[(i+1) % self.sizeY][j]))
+      a.append(((i+1) % self.sizeY,(j+1) % self.sizeX ,self.array[(i+1) % self.sizeY][(j+1) % self.sizeX]))
+      a.append(((i+1) % self.sizeY,k, self.array[(i+1) % self.sizeY][k]))
+      a.append((i,(j+1) % self.sizeX, self.array[i][(j+1) % self.sizeX]))
+      a.append((i,k, self.array[i][k]))
+    for elem in (set(self.point_list) | set(a)):
+        i = elem[0]
+        j = elem[1]
         t = self.sizeY -1 if i-1 == -1 else i-1
         k = self.sizeX -1 if j-1 == -1 else j-1
         wsum = self.array[t][j]                                   + \
@@ -115,23 +129,18 @@ class CellularGrid:
         state = self.model.check(self.array[i][j], wsum)
         if state != self.array[i][j]: 
           newpoints.append((i, j, state))
-          if i < self.startY: self.startY = i
-          if i > self.finalY: self.finalY = i
-          if j < self.startX: self.startX = j
-          if j > self.finalX: self.finalX = j
+    self.point_list = []
     for elem in newpoints:
       self.array[elem[0]][elem[1]] = elem[2]
+      self.point_list.append((elem[0], elem[1], elem[2]))
 
   def setPattern(self, pattern, off):
     """
     Resets the grid and sets a new pattern in the board.
     """
-    self._initBoardLimits()
+    self.point_list = []
     for point in pattern:
-      if point[0]+off[0] < self.startY: self.startY = point[0]+off[0]
-      if point[0]+off[0] > self.finalY: self.finalY = point[0]+off[0]
-      if point[1]+off[1] < self.startX: self.startX = point[1]+off[1]
-      if point[1]+off[1] > self.finalX: self.finalX = point[1]+off[1]
+      self.point_list.append((point[0]+off[0],point[1]+off[1], CellularGrid.ALIVE))
       self.array[point[0]+off[0]][point[1]+off[1]] = CellularGrid.ALIVE
 
   def getPatternCenterPosition(self, pattern):
--- main.py
+++ main.py
@@ -9,63 +9,59 @@ __maintainer__ = "Nunzio Meli"
 __email__      = "nunziomeli5@gmail.com"
 __status__     = "Production"
 
-import pygame
+
 import time
 import random
-import copy
 
 from liblina import GOLEvolutionModel, CellularGrid
 
 #
 # Constaints
 #
-SCALE = 2 # PIXELS SCALE FACTOR
-FPS   = 40 # FRAME RATE
+SCALE = 20 # PIXELS SCALE FACTOR
+FPS   = 300 # FRAME RATE
 
-#
-# Shows cellular grid to the screen
-#
-def render(screen, surface, portion, grid, scale, color_mode = None):
-  print(portion[0],portion[1],portion[2],portion[3])
-  for i in range(portion[0], portion[1]+1):
-    for j in range(portion[2], portion[3]+1):
-      color = (0,0,0) if grid[i][j] == 0 else (255,255,255)
-      if color_mode == "black":
-        color = (0,0,0)
-      pygame.draw.rect(surface, color, pygame.Rect(j*scale,i*scale,scale,scale))
-  screen.blit(surface, (0,0))
-  pygame.display.flip()
 
+class Automata:
+  #
+  # Patterns
+  #
+  PT_GLIDER        = [[0,1], [1,2], [2,0], [2,1], [2,2]]
+  PT_BLINKER       = [[1,1], [2,1], [3,1]]
+  PT_TRIO          = [[0,1], [1,0], [1,1], [1,2]]
+  PT_PULSAR        = [[0,0], [1,0], [2,0], [3,0], [4,0], [0,2], [4,2], [0,4], [1,4], [2,4], [3,4], [4,4]]
+
+  PT_GOSPEL        = [[5,1], [5,2], [6,1], [6,2], [5,11], [6,11], [7,11], [4,12], [8,12], [3,13], [3,14], 
+                      [9,13], [9,14], [6,15], [4,16], [8,16], [5,17], [6,17], [7,17], [6,18], [3,21], [4,21],
+                      [5,21], [3,22], [4,22], [5,22], [2,23], [6,23], [1,25], [2,25], [6,25], [7,25], [3,35], 
+                      [4,35], [3,36], [4,36]]
+
+  PT_PERIOD        = [[1,1], [17,1], [1,2], [15,2], [16,2], [17,2], [1,2], [2,2], [3,2], [4,3], [14,3], [3,4],
+                      [4,4], [14,4], [15,4], [5,12], [6,12], [7,12], [11,12], [12,12], [13,12], [5,13], [8,13],
+                      [10,13], [13,13], [5,14], [13,14], [6,16], [12,16], [7,17], [8,17], [10,17], [11,17],
+                      [3,26], [4,26], [3,27], [4,27], [14,26], [15,26], [14,27], [15,27], [7,36], [11,36], 
+                      [7,37], [11,37], [4,40], [5,40], [7,40], [11,40], [13,40], [14,40], [5,41], [6,41],
+                      [7,41], [11,41], [12,41], [13,41], [6,42], [12,42], [3,49], [4,49], [14,49], [15,49],
+                      [4,50], [14,50], [1,51], [2,51], [3,51], [15,51], [16,51], [17,51], [1,52], [17,52]]
+
+  def init(self, parent):
+    self.scene = [Automata.PT_GLIDER, Automata.PT_TRIO, Automata.PT_PULSAR, Automata.PT_GOSPEL, Automata.PT_PERIOD]
+    self.index = -1
+
+  def loop(self, parent):
+    parent.render()
+    parent.evolve()
+
+  def keyPressEvent(self, parent):
+    self.index = (self.index + 1) % len(self.scene)
+    parent.reset()
+    parent.setPattern(self.scene[self.index], parent.getPatternCenterPosition(self.scene[self.index]))
 
 #
 # Main
 #
 if __name__ == "__main__":
-  pygame.init()
-  screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-  width, height = screen.get_size()
-  surface       = pygame.Surface(screen.get_size())
-  cellular_grid = None
-  clock         = pygame.time.Clock()
-  index         = -1
-  fn = [GOLEvolutionModel.PT_GLIDER, 
-        GOLEvolutionModel.PT_TRIO, 
-        GOLEvolutionModel.PT_PULSAR, 
-        GOLEvolutionModel.PT_GOSPEL,
-        GOLEvolutionModel.PT_PERIOD]
-  while True:
-    if cellular_grid is not None: 
-      render(screen, surface,(cellular_grid.startY,cellular_grid.finalY,cellular_grid.startX,cellular_grid.finalX),cellular_grid.array, SCALE)
-      cellular_grid.evolve()
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
-          exit(0)
-        index = (index + 1) % len(fn)
-        cellular_grid = CellularGrid(GOLEvolutionModel(), width / SCALE, height / SCALE)
-        render(screen, surface,(0,cellular_grid.sizeY-1,0,cellular_grid.sizeX-1),cellular_grid.array, SCALE, "black")
-        cellular_grid.setPattern(fn[index], cellular_grid.getPatternCenterPosition(fn[index]))
-    clock.tick(FPS)
+  CellularGrid(Automata(), GOLEvolutionModel(), SCALE, FPS)
 
 
 
