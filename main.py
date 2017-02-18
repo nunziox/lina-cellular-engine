312833361fc0c8a92c79abe357fcf4d5645638d3
Marcello Romani <marcello.romani.enit@gmail.com>
2017-02-18 22:01:49 +0000

    add command line options for model, scale, frame rate

--- main.py
+++ main.py
@@ -9,9 +9,51 @@ __maintainer__ = "Nunzio Meli"
 __email__      = "nunziomeli5@gmail.com"
 __status__     = "Production"
 
+import sys
+import logging
+from argparse import ArgumentParser
 import json
 from os import stat
 from liblina import NoOpEvolutionModel, GOLEvolutionModel, CellularGrid
+
+
+def optSetup():
+    parser = ArgumentParser()
+
+    parser.add_argument("--fps",
+        type = int,
+        default = 30,
+        help = "animation frame rate (frames per second)")
+
+    parser.add_argument("--scale",
+        type = int,
+        default = 5,
+        help = "size of each cell (pixel)")
+
+    parser.add_argument("--model",
+        type = str,
+        choices = ["GOL", "NOP"],
+        default = "GOL",
+        help = "evolution model; GOL = Game Of Life, NOP = Null")
+
+    parser.add_argument("--log-level",
+        type = str,
+        choices = ["DEBUG", "INFO", "ERROR"],
+        default = "INFO",
+        help = "Set log level")
+
+    return parser
+
+
+def logSetup(logLevel):
+    rootLogger = logging.getLogger()
+    rootLogger.setLevel(logLevel)
+
+    logHandler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logHandler.setFormatter(formatter)
+    rootLogger.addHandler(logHandler)
+
 
 class Controller:
 
@@ -44,11 +86,29 @@ class Controller:
     with open(fileName) as fp:
       return json.load(fp)
 
+
+def main():
+    parser = optSetup()
+    args = parser.parse_args()
+    logSetup(logging.getLevelName(args.log_level))
+
+    logger=logging.getLogger("main")
+
+    scale = args.scale
+    fps = args.fps
+
+    if args.model == "GOL":
+        model = GOLEvolutionModel()
+    elif args.model == "NOP":
+        model = NoOpEvolutionModel()
+    else:
+        raise Exception("Unknown evolution model.")
+
+    CellularGrid(Controller(), model, scale, fps)
+
+
 #
 # Main
 #
 if __name__ == "__main__":
-  scale = 5  # Each pixel in the grid will be 20 x 20 screen pixels
-  fps   = 300 # FRAME RATE (the loop method inside the controller will be called each 1/FPS seconds)
-  CellularGrid(Controller(), GOLEvolutionModel(), scale, fps)
-  #CellularGrid(Controller(), NoOpEvolutionModel(), scale, fps)
+    main()
